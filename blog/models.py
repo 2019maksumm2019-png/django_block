833eb5abbc35a40e45e06702f89889840d73d22b
2019maksumm2019-png <2019maksumm2019>
2026-05-12 20:51:06 +0300

    Inital project commit

--- blog/models.py
+++ blog/models.py
@@ -8,8 +8,13 @@ class Post(models.Model):
     images = models.ImageField(upload_to='post_images/', null=True, blank=True)
     published_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"Пост: {self.title}"
 class Comment(models.Model):
     content = models.TextField()
     published_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE) # type: ignore
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Коментар від {self.author.username} до посту '{self.post.title}'"
